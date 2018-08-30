bc041bcda64dad8c6fb9bf3cb1bea3496e85adf2
wuhanyu5211 <215292699@qq.com>
2018-08-30 13:49:34 +0800

    添加了预加载图片和404页面

---
+++ src/components/page-not-found/page-not-found.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="page-not-found">
+      你访问的页面地址错误，请检查后从新输入地址。
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'page-not-found',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.page-not-found{
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  line-height: 300px;
+  background: #000000;
+  color: #fff;
+  text-align: center;
+  font-size: 30px;
+}
+</style>
